30b23b67cf93c7610b542188716888c71d373008
hamom <hamomel@gmail.com>
2019-09-29 22:38:09 +0600

    terjan implemented

--- demukron/src/demukron/Main.java
+++ demukron/src/demukron/Main.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	    int[][] graph = createAcyclicGraph(30);
+	    int[][] graph = createAcyclicGraph(10);
 
         for (int i = 0; i < graph.length; i++) {
             System.out.print(i + " ");
@@ -16,6 +16,13 @@ public class Main {
         }
 
 	    System.out.println(demukron(graph));
+	    int[] sorted = terjan(graph);
+
+	    for (int vertex : sorted) {
+	        System.out.print("[ " + vertex + " ]");
+        }
+
+        System.out.println();
     }
 
     private static VectorArray<VectorArray<Integer>> demukron(int[][] graph) {
@@ -53,6 +60,37 @@ public class Main {
         }
 
         return result;
+    }
+
+    private static int[] terjan(int[][] graph) {
+        int[] array = new int[graph.length];
+        VectorArray<Integer> stack = new VectorArray<Integer>(graph.length);
+
+        for (int i = 0; i < graph.length; i++) {
+            if (array[i] != 0) continue;
+            stack.add(Dfs(i, graph, array, stack));
+            array[i] = 2;
+        }
+
+        int place = array.length - 1;
+        for (int i = 0; i < stack.size(); i++) {
+            array[place] = stack.get(i);
+            place--;
+        }
+
+        return array;
+    }
+
+    private static int Dfs(int vertex, int[][] graph, int[] colored, VectorArray<Integer> stack) {
+        for (int edge : graph[vertex]) {
+            if (colored[edge] == 2) continue;
+            if (colored[edge] == 1) throw new IllegalStateException("there is a cycle in the graph");
+            colored[edge] = 1;
+            stack.add(Dfs(edge, graph, colored, stack));
+        }
+
+        colored[vertex] = 2;
+        return vertex;
     }
 
     private static int[][] createAcyclicGraph(int size) {
