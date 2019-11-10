a946f62efd72c4de9186d174e5c393fe66a0a0cc
hamom <hamomel@gmail.com>
2019-11-10 16:32:18 +0600

    xor encryption implemented

--- encrypter/src/main/main.kt
+++ encrypter/src/main/main.kt
@@ -1,2 +1,53 @@
 package main
 
+import java.io.File
+import kotlin.experimental.xor
+
+fun main() {
+
+}
+
+fun encrypt(rawFile: String, encryptedFile: String, key: String) {
+    val inFile = File(rawFile)
+    val outFile = File(encryptedFile)
+    xorFiles(inFile, outFile, key)
+}
+
+fun decrypt(encryptedFile: String, decryptedFile: String, key: String) {
+    val inFile = File(encryptedFile)
+    val outFile = File(decryptedFile)
+    xorFiles(inFile, outFile, key)
+    outFile.appendBytes("\n\n".toByteArray() + key.toByteArray())
+}
+
+private fun xorFiles(inFile: File, outFile: File, key: String) {
+    inFile.inputStream().use { input ->
+        val output = outFile.outputStream()
+        val buffer = ByteArray(key.length * 16)
+        val keyBytes = key.toByteArray()
+
+        while (true) {
+            println(input.available())
+            val read = input.read(buffer)
+            if (read <= 0) break
+            val enc = xorArray(buffer, keyBytes, read)
+            output.write(enc)
+        }
+        output.flush()
+        output.close()
+    }
+}
+
+private fun xorArray(text: ByteArray, key: ByteArray, size: Int): ByteArray {
+    val output = ByteArray(size)
+
+    for (i in 0..size / key.size) {
+        for (j in key.indices) {
+            val index = i * key.size + j
+            if (index == size) break
+            output[index] = text[index] xor key[j]
+        }
+    }
+
+    return output
+}
--- encrypter/src/test/Test.kt
+++ encrypter/src/test/Test.kt
@@ -1,4 +1,30 @@
 package test
 
+import main.decrypt
+import main.encrypt
+import org.junit.Test
+import java.io.File
+import kotlin.test.assertEquals
+
 class Test {
+    private val testDir = System.getProperty("user.dir") + "/testfiles"
+    private val originalFile = "$testDir/original.txt"
+    private val encryptedFile = "$testDir/encrypted.txt"
+    private val decryptedFile = "$testDir/decrypted.txt"
+    private val key ="Разработка программы, реализующей идею XOR шифрования"
+
+
+    @Test
+    fun testEncryptDecrypt() {
+        encrypt(originalFile, encryptedFile, key)
+        decrypt(encryptedFile, decryptedFile, key)
+
+        val originalText = File(originalFile).readLines()
+        val decryptedLines = File(decryptedFile).readLines()
+        val decryptedText = decryptedLines.take(decryptedLines.size - 2)
+        val decryptedKey = decryptedLines.last()
+
+        assertEquals(originalText, decryptedText)
+        assertEquals(key, decryptedKey)
+    }
 }
