9999c06fe039442463825c9ebe19e83a08ff0405
hamom <hamomel@gmail.com>
2019-11-24 18:14:13 +0600

    cache improved

--- cahe/src/main/App.java
+++ cahe/src/main/App.java
@@ -2,6 +2,6 @@ package main;
 
 public class App {
     public static void main(String[] args) {
-
+        System.out.println("Hello");
     }
 }
--- cahe/src/main/Cache.java
+++ cahe/src/main/Cache.java
@@ -5,6 +5,7 @@ public class Cache<K, V> {
     private int maxSize;
     private int size = 0;
     private Item head;
+    private Item tail;
 
     public Cache(int size, int timeout) {
         this.maxSize = size;
@@ -12,23 +13,28 @@ public class Cache<K, V> {
     }
 
     public V get(K key) {
-        if (head.key == key) {
-            head.lastAccess = System.currentTimeMillis();
-            return head.value;
-        }
-
-        Item previous = head;
-        Item current = head.next;
+        Item current = head;
         while (current != null) {
+            // if desired item is found, move it to the start of the list and update it's access time,
+            // so that sort the list by access time and make most often used items accessible faster
             if (current.key.equals(key)) {
-                previous.next = current.next;
-                current.next = head;
-                head = current;
+                // we only need to move the item if it is not the head
+                if (!current.equals(head)) {
+                    current.previous.next = current.next;
+                    if (current.equals(tail)) {
+                        tail = current.previous;
+                    } else {
+                        current.next.previous = current.previous;
+                    }
+                    current.next = head;
+                    head.previous = current;
+                    current.previous = null;
+                    head = current;
+                }
                 current.lastAccess = System.currentTimeMillis();
                 return current.value;
             }
 
-            previous = current;
             current = current.next;
         }
 
@@ -36,58 +42,44 @@ public class Cache<K, V> {
     }
 
     public void set(K key, V value) {
-        if (size == maxSize && !checkTimeout()) {
-            dropLast();
+        if (size == maxSize && !dropOutdated()) {
+            dropTail();
         }
+
         Item newItem = new Item(key, value);
+        // if cache is empty set new item as a tail either, otherwise link current head to the new item
+        if (head == null) {
+            tail = newItem;
+        } else {
+            head.previous = newItem;
+        }
         newItem.next = head;
         head = newItem;
         size++;
-        checkTimeout();
+        // drop outdated items to keep cache clean. Might be it is not needed (depends on purpose of cache)
+        dropOutdated();
     }
 
-    private boolean checkTimeout() {
+    private boolean dropOutdated() {
         boolean isFreed = false;
 
-        if (System.currentTimeMillis() - head.lastAccess > timeout) {
-            Item next = head.next;
-            head.next = null;
-            head = next;
+        // the items are sorted by lastAccess by design, so we can check them in a sequence from the tail
+        while (tail != null && System.currentTimeMillis() - tail.lastAccess > timeout) {
+            dropTail();
             isFreed = true;
-            size--;
-        }
-        Item previous = head;
-        Item current = head.next;
-        while (current != null) {
-            if (System.currentTimeMillis() - current.lastAccess > timeout) {
-                Item next = current.next;
-                current.next = null;
-                previous.next = next;
-                current = next;
-                isFreed = true;
-                size--;
-            } else {
-                previous = current;
-                current = current.next;
-            }
         }
         return isFreed;
     }
 
-    private void dropLast() {
-        Item previous = head;
-        Item current = head.next;
-        while (current.next != null) {
-            previous = current;
-            current = current.next;
-        }
-
-        previous.next = null;
+    private void dropTail() {
+        tail.previous.next = null;
+        tail = tail.previous;
         size--;
     }
 
     private class Item {
         long lastAccess;
+        Item previous;
         Item next;
         private K key;
         private V value;
@@ -98,12 +90,9 @@ public class Cache<K, V> {
             lastAccess = System.currentTimeMillis();
         }
 
-        public V getValue() {
-            return value;
-        }
-
-        public K getKey() {
-            return key;
+        @Override
+        public String toString() {
+            return "[" + key + " : " + value + "]";
         }
     }
 }
